6e28ba8b681861599f023c306894d052bb9f40ab
Andrzej Wrzesiński <andrzejwrzesinski.2004@gmail.com>
2025-12-12 00:32:35 +0100

    Created loop paths buttons

---
+++ vocal_looper_app/lib/loop_path.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+class LoopPath extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 300,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          SizedBox(
+            width: 80,
+            height: 80,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color.fromARGB(255, 0, 0, 0),
+                shape: CircleBorder(
+                  side: BorderSide(
+                    color: Color.fromARGB(255, 61, 62, 63),
+                    width: 2,
+                  ),
+                ),
+                padding: EdgeInsets.zero,
+              ),
+              onPressed: /*TODO*/ () => {},
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(
+                    Icons.play_arrow_rounded,
+                    color: Color.fromARGB(255, 61, 62, 63),
+                    size: 30,
+                  ),
+                  Text(
+                    '/',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 61, 62, 63),
+                      fontSize: 30,
+                    ),
+                  ),
+                  Icon(
+                    Icons.pause_circle_filled_rounded,
+                    color: Color.fromARGB(255, 61, 62, 63),
+                    size: 30,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 50,
+            height: 50,
+            child: IconButton(
+              style: IconButton.styleFrom(
+                backgroundColor: Color.fromARGB(255, 0, 0, 0),
+                shape: CircleBorder(
+                  side: BorderSide(
+                    color: Color.fromARGB(255, 61, 62, 63),
+                    width: 2,
+                  ),
+                ),
+                padding: EdgeInsets.zero,
+              ),
+              onPressed: /*TODO*/ () => {},
+              icon: Icon(
+                Icons.square_rounded,
+                color: Color.fromARGB(255, 126, 9, 9),
+                size: 30,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 30,
+            width: 60,
+            child: ElevatedButton(
+              style: TextButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 11, 95, 27),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                padding: EdgeInsets.all(3),
+              ),
+              onPressed: /*TODO*/ () => {},
+              child: Text(
+                'Edit',
+                style: TextStyle(
+                  color: Color.fromARGB(255, 255, 255, 255),
+                  fontSize: 18,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- vocal_looper_app/lib/main.dart
+++ vocal_looper_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './loop_path.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,10 +10,32 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      title: 'Vocal Looper',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: const Color.fromARGB(255, 13, 13, 19),
+        colorScheme: const ColorScheme.dark(
+          primary: Color(0xFF58A6FF),
+          secondary: Color(0xFF7EE787),
+          surface: Color(0xFF161B22),
+          error: Color(0xFFF85149),
+        ),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color.fromARGB(255, 17, 17, 25),
+          elevation: 0,
+        ),
+      ),
       home: Scaffold(
+        appBar: AppBar(title: Text('Vocal Looper')),
         body: Center(
-          child: Text('Hello World!'),
+          child: SizedBox(
+            height: 700,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [for (int i = 0; i < 5; i++) LoopPath()],
+            ),
+          ),
         ),
       ),
     );
