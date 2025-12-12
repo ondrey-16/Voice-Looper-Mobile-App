89dcdf167dc2a3b78a2d28d865220586281ebb56
Andrzej Wrzesiński <andrzejwrzesinski.2004@gmail.com>
2025-12-12 02:58:28 +0100

    Added gradient and changed colors of buttons in loop path

--- vocal_looper_app/lib/loop_path.dart
+++ vocal_looper_app/lib/loop_path.dart
@@ -11,84 +11,116 @@ class LoopPath extends StatelessWidget {
           SizedBox(
             width: 80,
             height: 80,
-            child: ElevatedButton(
-              clipBehavior: Clip.antiAlias,
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Color.fromARGB(255, 0, 0, 0),
-                shape: CircleBorder(
-                  side: BorderSide(
-                    color: Color.fromARGB(255, 61, 62, 63),
-                    width: 2,
-                  ),
+            child: Container(
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                gradient: LinearGradient(
+                  begin: Alignment.bottomLeft,
+                  end: Alignment.topRight,
+                  colors: [
+                    Color.fromARGB(255, 0, 0, 0),
+                    Color.fromARGB(255, 44, 43, 43),
+                  ],
                 ),
-                padding: EdgeInsets.zero,
+                border: Border.all(
+                  color: Color.fromARGB(255, 53, 54, 54),
+                  width: 2,
+                ),
               ),
-              onPressed: /*TODO*/ () => {},
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Icon(
-                    Icons.play_arrow_rounded,
-                    color: Color.fromARGB(255, 61, 62, 63),
-                    size: 30,
-                  ),
-                  Text(
-                    '/',
-                    style: TextStyle(
+              child: ElevatedButton(
+                clipBehavior: Clip.antiAlias,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.transparent,
+                  padding: EdgeInsets.zero,
+                ),
+                onPressed: /*TODO*/ () => {},
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(
+                      Icons.play_arrow_rounded,
                       color: Color.fromARGB(255, 61, 62, 63),
-                      fontSize: 30,
+                      size: 30,
                     ),
-                  ),
-                  Icon(
-                    Icons.pause_circle_filled_rounded,
-                    color: Color.fromARGB(255, 61, 62, 63),
-                    size: 30,
-                  ),
-                ],
+                    Text(
+                      '/',
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 61, 62, 63),
+                        fontSize: 30,
+                      ),
+                    ),
+                    Icon(
+                      Icons.pause_circle_filled_rounded,
+                      color: Color.fromARGB(255, 61, 62, 63),
+                      size: 30,
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
           SizedBox(
             width: 50,
             height: 50,
-            child: ElevatedButton(
-              clipBehavior: Clip.antiAlias,
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Color.fromARGB(255, 0, 0, 0),
-                shape: CircleBorder(
-                  side: BorderSide(
-                    color: Color.fromARGB(255, 61, 62, 63),
-                    width: 2,
-                  ),
+            child: Container(
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                gradient: LinearGradient(
+                  begin: Alignment.bottomLeft,
+                  end: Alignment.topRight,
+                  colors: [
+                    Color.fromARGB(255, 0, 0, 0),
+                    Color.fromARGB(255, 44, 43, 43),
+                  ],
                 ),
-                padding: EdgeInsets.zero,
               ),
-              onPressed: /*TODO*/ () => {},
-              child: Icon(
-                Icons.square_rounded,
-                color: Color.fromARGB(255, 126, 9, 9),
-                size: 30,
+              child: ElevatedButton(
+                clipBehavior: Clip.antiAlias,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.transparent,
+                  padding: EdgeInsets.zero,
+                ),
+                onPressed: /*TODO*/ () => {},
+                child: Icon(
+                  Icons.square_rounded,
+                  color: Color.fromARGB(255, 126, 9, 9),
+                  size: 25,
+                ),
               ),
             ),
           ),
           SizedBox(
             height: 30,
             width: 60,
-            child: ElevatedButton(
-              clipBehavior: Clip.antiAlias,
-              style: TextButton.styleFrom(
-                backgroundColor: const Color.fromARGB(255, 11, 95, 27),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
+            child: Container(
+              decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(10),
+                gradient: LinearGradient(
+                  begin: Alignment.bottomLeft,
+                  end: Alignment.topRight,
+                  colors: [
+                    Color.fromARGB(255, 3, 54, 15),
+                    Color.fromARGB(255, 2, 188, 39),
+                  ],
                 ),
-                padding: EdgeInsets.all(3),
               ),
-              onPressed: /*TODO*/ () => {},
-              child: Text(
-                'Edit',
-                style: TextStyle(
-                  color: Color.fromARGB(255, 255, 255, 255),
-                  fontSize: 18,
+              child: ElevatedButton(
+                clipBehavior: Clip.antiAlias,
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.transparent,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadiusGeometry.circular(10),
+                  ),
+                  padding: EdgeInsets.all(3),
+                ),
+                onPressed: /*TODO*/ () => {},
+                child: Text(
+                  'Edit',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 255, 255, 255),
+                    fontSize: 18,
+                  ),
                 ),
               ),
             ),
