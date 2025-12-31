af8faece4163dcafe1bedf8ca74419aed6a7345d
Ondrey <andrzejwrzesinski.2004@gmail.com>
2025-12-31 16:05:53 +0100

    Changed birth date form field. Form widget moved to new class

--- vocal_looper_app/lib/sign_up_page.dart
+++ vocal_looper_app/lib/sign_up_page.dart
@@ -12,64 +12,85 @@ class SignUpPage extends StatelessWidget {
         ),
         title: Text('Sign up'),
       ),
-      body: Form(
-        child: ListView(
-          padding: EdgeInsets.all(30),
-          children: [
-            SeparatedWidget(
-              widget: TextFormField(
-                decoration: InputDecoration(labelText: 'Name'),
-              ),
+      body: SignUpForm(),
+    );
+  }
+}
+
+class SignUpForm extends StatelessWidget {
+  final _textController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: ListView(
+        padding: EdgeInsets.all(30),
+        children: [
+          SeparatedWidget(
+            widget: TextFormField(
+              decoration: InputDecoration(labelText: 'Name'),
             ),
-            SeparatedWidget(
-              widget: TextFormField(
-                decoration: InputDecoration(labelText: 'Last Name'),
-              ),
+          ),
+          SeparatedWidget(
+            widget: TextFormField(
+              decoration: InputDecoration(labelText: 'Last Name'),
             ),
-            SeparatedWidget(
-              widget: TextFormField(
-                decoration: InputDecoration(labelText: 'Nickname'),
-              ),
+          ),
+          SeparatedWidget(
+            widget: TextFormField(
+              decoration: InputDecoration(labelText: 'Nickname'),
             ),
-            SeparatedWidget(
-              widget: TextFormField(
-                decoration: InputDecoration(labelText: 'Email'),
-                keyboardType: TextInputType.emailAddress,
-              ),
+          ),
+          SeparatedWidget(
+            widget: TextFormField(
+              decoration: InputDecoration(labelText: 'Email'),
+              keyboardType: TextInputType.emailAddress,
             ),
-            /*TODO Change birth date form field*/
-            SeparatedWidget(
-              widget: TextFormField(
-                decoration: InputDecoration(labelText: 'Birth Date'),
-                keyboardType: TextInputType.datetime,
-              ),
+          ),
+          SeparatedWidget(
+            widget: TextFormField(
+              controller: _textController,
+              readOnly: true,
+              decoration: InputDecoration(labelText: 'Birth Date'),
+              onTap: () async {
+                DateTime? date = await showDatePicker(
+                  context: context,
+                  firstDate: DateTime(1900),
+                  lastDate: DateTime.now(),
+                  initialDate: DateTime.now(),
+                );
+
+                if (date != null) {
+                  _textController.text = date.toString().split(" ")[0];
+                }
+              },
             ),
-            SeparatedWidget(
-              widget: TextFormField(
-                decoration: InputDecoration(labelText: 'Password'),
-                obscureText: true,
-              ),
+          ),
+          SeparatedWidget(
+            widget: TextFormField(
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
             ),
-            SeparatedWidget(
-              widget: TextFormField(
-                decoration: InputDecoration(labelText: 'Confirm password'),
-                obscureText: true,
-              ),
+          ),
+          SeparatedWidget(
+            widget: TextFormField(
+              decoration: InputDecoration(labelText: 'Confirm password'),
+              obscureText: true,
             ),
-            Center(
-              child: SizedBox(
-                width: 90,
-                child: ElevatedButton(
-                  onPressed: () => context.go('/'),
-                  child: Text(
-                    'Send',
-                    style: TextStyle(fontSize: 18, color: Colors.white),
-                  ),
+          ),
+          Center(
+            child: SizedBox(
+              width: 90,
+              child: ElevatedButton(
+                onPressed: () => context.go('/'),
+                child: Text(
+                  'Send',
+                  style: TextStyle(fontSize: 18, color: Colors.white),
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
